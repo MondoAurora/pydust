07d091fb9fb178f292ca9c6c44443c79ea2ae36c
Zsolt Horváth <zsolt.horvath@real-digital.de>
2023-03-20 07:56:43 +0100

    Version 0.0.27

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="PyDust",
-    version="0.0.26",
+    version="0.0.27",
     description="PyDust",
     long_description=README,
     long_description_content_type="text/markdown",
